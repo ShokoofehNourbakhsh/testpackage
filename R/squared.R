4349ff260be13880ea53ac7ad5048e666efe6a7b
ShokoofehNourbakhsh <sho.nourbakhsh@gmail.com>
2021-08-03 11:57:20 -0400

    added test package

---
+++ R/squared.R
@@ -0,0 +1,14 @@
+#' Get square of a real number
+#' This function take a real number and square the value.
+#'
+#' @param x
+#'
+#' @return squared number
+#' @export
+#'
+#' @examples
+#' squared(12)
+squared <- function(x){
+  y = x^2
+  return(y)
+}
